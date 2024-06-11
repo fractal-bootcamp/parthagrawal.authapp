fde344308d021d200104b5454a63cf298f73d7ac
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-11 17:55:36 -0400

    basic server running

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("server is saying hello world!")
+})
+const PORT = 4000
+app.listen(PORT, () => {
+    console.log("server running on port http://localhost:" + PORT)
+})
+
+app.get('/posts', (req, res) => {
+    res.send("some posts")
+})
